7f48c57d1e50e263b64b9f087f596d35bf433c5f
Sebastian Muchui K <Sebastianmuchui79@gmail.com>
2022-10-21 18:44:28 +0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+#define IR 2
+#define pir 4
+#define servo 6
+Servo enter;
+void setup()
+{
+	pinMode(IR,INPUT);
+  	pinMode(pir,INPUT);
+	enter.attach(servo);
+}
+
+void loop()
+{
+  if(motionDetected() && digitalRead(IR)== HIGH){
+  	enter.write(90);
+  }
+}
+bool motionDetected(){
+  if(digitalRead(pir)== HIGH){
+  	return true;
+  }else{
+  	return false;
+  }  	
+}
+
